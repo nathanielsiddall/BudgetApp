8d066a1186c21a4e47792f93407ef764014ae377
wsiddall <nathaniel.siddall@gmail.com>
2018-10-09 16:30:45 -0500

    New BudgetApp
    this is the bedrock for the new budgetting app

---
+++ src/main/java/com/budgetapp/budgetapp/BudgetappApplication.java
@@ -0,0 +1,12 @@
+package com.budgetapp.budgetapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BudgetappApplication.class, args);
+    }
+}
